4e554120dbcf439cb9287eb1a6591dbad2368cd8
blahkesto <blakesto@gmail.com>
2017-01-24 21:10:54 -0800

    can now move snake

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.properties import ObjectProperty, ListProperty
 class SnekChunk(Widget):
     def __init__(self, chunk_x, chunk_y, **kwargs):
         super().__init__(**kwargs)
+        self.chunk_x = chunk_x
+        self.chunk_y = chunk_y
         self.pos = chunk_x * 25, chunk_y * 25
 
 
@@ -16,12 +18,28 @@ class Snek(Widget):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.chunks = [SnekChunk(2,2), SnekChunk(3,2)]
+        self.chunks = [SnekChunk(3,2), SnekChunk(2,2)]
 
     def on_chunks(self, *largs):
         self.clear_widgets()
         for widge in self.chunks:
             self.add_widget(widge)
+
+    def move(self, direction):
+        self.chunks.pop()
+        new_chunk_x = self.chunks[0].chunk_x
+        new_chunk_y = self.chunks[0].chunk_y
+        #FIXME: don't allow reverse
+        #FIXME: detect collision
+        if direction == "left":
+            new_chunk_x -= 1
+        elif direction == "right":
+            new_chunk_x += 1
+        elif direction == "down":
+            new_chunk_y -= 1
+        else:
+            new_chunk_y += 1
+        self.chunks.insert(0, SnekChunk(new_chunk_x, new_chunk_y))
 
 
 class SnekGame(Widget):
@@ -32,13 +50,13 @@ class SnekGame(Widget):
         x = touch.x / self.width * 100.0
         y = touch.y / self.height * 100.0
         if x < 33.3 and 33.3 < y < 66.6:
-            print("left")
+            self.snek.move("left")
         elif x > 66.6 and 33.3 < y < 66.6:
-            print("right")
+            self.snek.move("right")
         elif y < 33.3 and 33.3 < x < 66.6:
-            print("down")
+            self.snek.move("down")
         elif y > 66.6 and 33.3 < x < 66.6:
-            print("up")
+            self.snek.move("up")
 
 
 class SnekApp(App):
